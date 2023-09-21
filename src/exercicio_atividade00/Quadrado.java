461e3fcc71b364831561dfc29d49e44114a4216d
Josenilson Pereira <josenilson.pereira@solutis.com.br>
2023-09-21 10:42:54 -0300

    Adicionado novos programas

---
+++ src/exercicio_atividade00/Quadrado.java
@@ -0,0 +1,13 @@
+package exercicio_atividade00;
+
+public class Quadrado {
+	
+    public static void main(String[] args) {
+    	
+        for (int numero = 0; numero <= 10; numero++) {
+            int quadrado = numero * numero;
+            
+            System.out.println("O quadrado de " + numero + " x " + numero +  "=" + " " + quadrado );
+        }
+    }
+}
