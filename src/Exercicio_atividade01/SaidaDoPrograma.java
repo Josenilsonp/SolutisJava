4f246adc2febc72f9f6d22264c2886fefd86e876
Josenilson Pereira <josenilson.pereira@solutis.com.br>
2023-09-22 14:10:12 -0300

    Adicionado exercicio Saida do programa

--- src/Exercicio_atividade01/SaidaDoPrograma.java
+++ src/Exercicio_atividade01/SaidaDoPrograma.java
@@ -2,9 +2,15 @@ package Exercicio_atividade01;
 
 public class SaidaDoPrograma {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main( String args[] ){
+		int i = 5;
+		int j = i;
+		j = 10;
+		
+		/*
+		 * R: O resultado final do programa acima é um erro  devido a utilização do &quot 
+		   &quot; é a sequência de escape em HTML que representa as aspas duplas (").
+		 * */
+		System.out.println(&quot;i + j = &quot; + j + i );
+		}
 }
